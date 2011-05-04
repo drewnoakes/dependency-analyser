1ea51d0de17651dc11ff5b0c844ba1bff0e68e0a
Drew Noakes <git@drewnoakes.com>
2011-05-04 15:32:14 +0000

    Updated filter window size. Updated screenshots, replacing old menu-exclude.png with new filter-window.png.
    git-svn-id: https://dependency-analyser.googlecode.com/svn/trunk@17 8aec1112-6466-6e08-e931-4141cac027a1

--- Source/Drew.DependencyAnalyser/FilterForm.Designer.cs
+++ Source/Drew.DependencyAnalyser/FilterForm.Designer.cs
@@ -30,7 +30,6 @@ namespace Drew.DependencyAnalyser
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FilterForm));
             this._btnSelectNone = new System.Windows.Forms.Button();
             this._btnSelectAll = new System.Windows.Forms.Button();
             this._btnCancel = new System.Windows.Forms.Button();
@@ -38,12 +37,12 @@ namespace Drew.DependencyAnalyser
             this._tree = new Drew.DependencyAnalyser.Controls.TriStateTreeView();
             this.SuspendLayout();
             // 
-            // _btnRemoveAll
+            // _btnSelectNone
             // 
             this._btnSelectNone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this._btnSelectNone.Location = new System.Drawing.Point(103, 305);
+            this._btnSelectNone.Location = new System.Drawing.Point(93, 414);
             this._btnSelectNone.Name = "_btnSelectNone";
-            this._btnSelectNone.Size = new System.Drawing.Size(85, 23);
+            this._btnSelectNone.Size = new System.Drawing.Size(75, 23);
             this._btnSelectNone.TabIndex = 9;
             this._btnSelectNone.Text = "Select &None";
             this._btnSelectNone.UseVisualStyleBackColor = true;
@@ -52,9 +51,9 @@ namespace Drew.DependencyAnalyser
             // _btnSelectAll
             // 
             this._btnSelectAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this._btnSelectAll.Location = new System.Drawing.Point(12, 305);
+            this._btnSelectAll.Location = new System.Drawing.Point(12, 414);
             this._btnSelectAll.Name = "_btnSelectAll";
-            this._btnSelectAll.Size = new System.Drawing.Size(85, 23);
+            this._btnSelectAll.Size = new System.Drawing.Size(75, 23);
             this._btnSelectAll.TabIndex = 8;
             this._btnSelectAll.Text = "Select &All";
             this._btnSelectAll.UseVisualStyleBackColor = true;
@@ -64,7 +63,7 @@ namespace Drew.DependencyAnalyser
             // 
             this._btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this._btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this._btnCancel.Location = new System.Drawing.Point(320, 305);
+            this._btnCancel.Location = new System.Drawing.Point(261, 414);
             this._btnCancel.Name = "_btnCancel";
             this._btnCancel.Size = new System.Drawing.Size(75, 23);
             this._btnCancel.TabIndex = 14;
@@ -75,7 +74,7 @@ namespace Drew.DependencyAnalyser
             // 
             this._btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this._btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this._btnOK.Location = new System.Drawing.Point(239, 305);
+            this._btnOK.Location = new System.Drawing.Point(180, 414);
             this._btnOK.Name = "_btnOK";
             this._btnOK.Size = new System.Drawing.Size(75, 23);
             this._btnOK.TabIndex = 15;
@@ -92,7 +91,7 @@ namespace Drew.DependencyAnalyser
             this._tree.Location = new System.Drawing.Point(12, 12);
             this._tree.Name = "_tree";
             this._tree.SelectedImageIndex = 1;
-            this._tree.Size = new System.Drawing.Size(383, 287);
+            this._tree.Size = new System.Drawing.Size(324, 396);
             this._tree.TabIndex = 16;
             // 
             // FilterForm
@@ -102,7 +101,7 @@ namespace Drew.DependencyAnalyser
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.CancelButton = this._btnCancel;
-            this.ClientSize = new System.Drawing.Size(407, 334);
+            this.ClientSize = new System.Drawing.Size(348, 444);
             this.Controls.Add(this._tree);
             this.Controls.Add(this._btnOK);
             this.Controls.Add(this._btnCancel);
@@ -110,7 +109,7 @@ namespace Drew.DependencyAnalyser
             this.Controls.Add(this._btnSelectAll);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.MaximizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(373, 144);
+            this.MinimumSize = new System.Drawing.Size(364, 150);
             this.Name = "FilterForm";
             this.Text = "Filter";
             this.ResumeLayout(false);
