845760c21397c4ab79bf2adb2062873f7ec48872
Drew Noakes <git@drewnoakes.com>
2015-10-17 21:34:32 +0100

    Add method to perform transitive reduction of DependencyGraph.

--- Source/Drew.DependencyAnalyser.Tests/DependencyGraphTest.cs
+++ Source/Drew.DependencyAnalyser.Tests/DependencyGraphTest.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using System.Linq;
 
 namespace Drew.DependencyAnalyser.Tests
 {
@@ -12,8 +11,57 @@ namespace Drew.DependencyAnalyser.Tests
             var graph = new DependencyGraph<string>();
             graph.AddDependency("A", "B");
 
-            CollectionAssert.AreEqual(new[] { "A", "B" }, graph.Nodes.ToArray());
-            CollectionAssert.AreEqual(new[] { "B" }, graph.GetDependenciesForNode("A").ToArray());
+            CollectionAssert.AreEqual(new[] { "A", "B" }, graph.Nodes);
+            CollectionAssert.AreEqual(new[] { "B" }, graph.GetDependenciesForNode("A"));
+        }
+
+        [Test]
+        public void TransitiveReduceThreeNodes()
+        {
+            var graph = new DependencyGraph<char>();
+            graph.AddDependency('a', 'b');
+            graph.AddDependency('b', 'c');
+            graph.AddDependency('a', 'c');
+
+            graph.TransitiveReduce();
+
+            CollectionAssert.AreEqual(new[] { 'a', 'b', 'c' }, graph.Nodes);
+            CollectionAssert.AreEqual(new[] { 'b' }, graph.GetDependenciesForNode('a'));
+            CollectionAssert.AreEqual(new[] { 'c' }, graph.GetDependenciesForNode('b'));
+        }
+
+        [Test]
+        public void TransitiveReduceCascade()
+        {
+            var graph = new DependencyGraph<char>();
+
+            for (var c1 = 'a'; c1 < 'f'; c1++)
+                for (var c2 = (char)(c1 + 1); c2 < 'f'; c2++)
+                    graph.AddDependency(c1, c2);
+
+            graph.TransitiveReduce();
+
+            for (var c = 'a'; c < 'f' - 1; c++)
+                CollectionAssert.AreEqual(new[] { (char)(c + 1) }, graph.GetDependenciesForNode(c), $"Dep of {c} should be {(char)(c+1)}");
+        }
+
+        [Test]
+        public void TransitiveReduceCycles()
+        {
+            var graph = new DependencyGraph<char>();
+
+            graph.AddDependency('a', 'b'); // a <--> b
+            graph.AddDependency('b', 'a');
+
+            graph.AddDependency('a', 'c'); // a --> c
+            graph.AddDependency('b', 'c'); // b --> c
+
+            graph.TransitiveReduce();
+
+            // NOTE result here depends upon order of enumeration from HashSet<char>, making this test potentially fragile
+
+            CollectionAssert.AreEqual(new[] { 'b' }, graph.GetDependenciesForNode('a'));
+            CollectionAssert.AreEqual(new[] { 'a', 'c' }, graph.GetDependenciesForNode('b'));
         }
     }
 }
--- Source/Drew.DependencyAnalyser/DependencyGraph.cs
+++ Source/Drew.DependencyAnalyser/DependencyGraph.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Drew.DependencyAnalyser
@@ -33,6 +34,63 @@ namespace Drew.DependencyAnalyser
             return _dependenciesByNode.TryGetValue(dependant, out dependencies)
                        ? dependencies
                        : Enumerable.Empty<T>();
+        }
+
+        /// <summary>
+        /// Remove edges from the graph without changing the reachability of nodes.
+        /// If a node has both a direct and transitive dependency upon another node, the direct dependency is removed.
+        /// </summary>
+        public void TransitiveReduce()
+        {
+            var visited = new HashSet<T>();
+            var frontier = new Stack<T>();
+
+            foreach (var root in _nodes)
+            {
+                HashSet<T> children;
+                if (!_dependenciesByNode.TryGetValue(root, out children))
+                    continue;
+
+                var childList = children.ToList();
+                for (var i = 0; i < childList.Count; i++)
+                {
+                    var child = childList[i];
+                    Debug.Assert(!frontier.Any());
+
+                    visited.Clear();
+                    visited.Add(root);
+
+                    // Perform a DFS from this child
+                    frontier.Push(child);
+
+                    while (frontier.Any())
+                    {
+                        var node = frontier.Pop();
+
+                        // If we've already expanded this node, continue
+                        if (!visited.Add(node))
+                            continue;
+
+                        foreach (var dependency in GetDependenciesForNode(node))
+                            frontier.Push(dependency);
+
+                        if (!Equals(child, node) && children.Contains(node))
+                        {
+                            // This node is directly linked to from root,
+                            // however we arrived here via a longer, indirect path
+                            // so we remove the shorter, direct path
+                            children.Remove(node);
+                            
+                            // Update the list we're iterating over, adjusting the current index if required
+                            var index = childList.IndexOf(node);
+                            Debug.Assert(index != -1);
+                            childList.RemoveAt(index);
+                            if (index <= i)
+                                i--;
+                        }
+                    }
+                }
+            }
         }
     }
 }
